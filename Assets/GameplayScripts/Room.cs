79ce4c1360195389a737670be72772b7ebef5050
CihanRSN <cihanrasna@gmail.com>
2022-09-07 12:33:55 +0300

    Room manager & room prefab

---
+++ Assets/GameplayScripts/Room.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace GameplayScripts
+{
+    public class Room : MonoBehaviour
+    {
+        public bool isActiveRoom;
+        [SerializeField] private List<GameObject> walls;
+        [SerializeField] private List<Room> neighborRooms = new(4);
+
+        private int _roomID;
+        private int _roomUniqueID;
+
+        public void GetIDFromManager(int id, bool isActiveRoom)
+        {
+            this.isActiveRoom = isActiveRoom;
+            _roomID = id;
+            _roomUniqueID = $"{gameObject.name},{_roomID.ToString()}".GetHashCode();
+
+            gameObject.SetActive(isActiveRoom);
+        }
+
+        public void GetNeighborRooms(Room neighborRoom)
+        {
+            if (!neighborRooms.Contains(neighborRoom))
+            {
+                neighborRooms.Add(neighborRoom);
+            }
+
+            if (neighborRoom.isActiveRoom)
+            {
+                var orderByDist = walls.OrderBy(w =>
+                    Vector3.Distance(neighborRoom.transform.position, w.transform.position));
+                var asd = orderByDist.First();
+                var dist = Vector3.Distance(asd.transform.position, neighborRoom.transform.position);
+                if (dist <= GetComponent<MeshRenderer>().bounds.extents.magnitude)
+                {
+                    asd.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
--- Assets/RSNManagers/RoomManager.cs
+++ Assets/RSNManagers/RoomManager.cs
@@ -1,6 +1,118 @@
+using System.Collections.Generic;
+using GameplayScripts;
+using UnityEngine;
+
 namespace RSNManagers
 {
     public class RoomManager : Singleton<RoomManager>
     {
+        [SerializeField] private Room roomPrefab;
+        [SerializeField] private Transform roomHolder;
+        [SerializeField] private List<Room> roomsOnScene;
+        [SerializeField] private List<Room> currentlyActiveRooms;
+        [SerializeField] private int horizontalRoomCount = 3;
+        [SerializeField] private int verticalRoomCount = 5;
+
+
+        protected override void Awake()
+        {
+            base.Awake();
+            InstantiateRoomPrefabs();
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+            var activeRoomCount = PersistManager.Instance.ActiveRoomCount;
+            ActivateRooms(activeRoomCount);
+            FindNeighborRooms();
+        }
+
+        private void InstantiateRoomPrefabs()
+        {
+            var count = 1;
+            string posName = null;
+            var desiredPos = Vector3.zero;
+            for (var i = 0; i < verticalRoomCount; i++)
+            {
+                desiredPos.z = i * 15f;
+                for (var j = 0; j < horizontalRoomCount; j++)
+                {
+                    if (j % 2 == 0)
+                    {
+                        desiredPos.x = j * 7.5f;
+                        posName = $"[0,{i + 1},1]";
+                    }
+                    else
+                    {
+                        desiredPos.x = (-j - 1) * 7.5f;
+                        posName = $"[1,{i + 1},0]";
+                    }
+
+
+                    var room = Instantiate(roomPrefab, desiredPos, Quaternion.identity, roomHolder);
+                    room.name = $"Room {posName}    Order : {count}";
+                    roomsOnScene.Add(room);
+                    count += 1;
+                }
+            }
+        }
+
+        private void ActivateRooms(int activeRoomCount)
+        {
+            for (var i = 0; i < roomsOnScene.Count; i++)
+            {
+                var currentRoom = roomsOnScene[i];
+                currentRoom.GetIDFromManager(i, i < activeRoomCount);
+                if (i < activeRoomCount)
+                {
+                    currentlyActiveRooms.Add(currentRoom);
+                }
+            }
+        }
+
+        private void FindNeighborRooms()
+        {
+            for (var i = 0; i < roomsOnScene.Count; i++)
+            {
+                var neighborArray = new float?[4];
+                var currentRoom = roomsOnScene[i];
+                for (var j = 0; j < roomsOnScene.Count; j++)
+                {
+                    if (j == i) continue;
+
+                    var possibleNeighbor = roomsOnScene[j];
+                    var dist = Vector3.Distance(currentRoom.transform.position, possibleNeighbor.transform.position);
+                    for (var k = 0; k < neighborArray.Length; k++)
+                    {
+                        //Debug.Log(currentRoom.gameObject.name + " : ITERATED");
+                        if (neighborArray[k] == null)
+                        {
+                            neighborArray[k] = dist;
+                        }
+                        else if (neighborArray[k].HasValue && dist <= neighborArray[k].Value)
+                        {
+                            neighborArray[k] = dist;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+
+                        currentRoom.GetNeighborRooms(possibleNeighbor);
+                    }
+                }
+            }
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+        }
     }
 }
